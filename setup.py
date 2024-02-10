cb5793315403b2e654aaa9e26623fffada7af137
Sanjay Singh <sanjaysingh@Sanjays-MacBook-Pro.local>
2024-02-10 12:51:39 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]  # Using strip() to remove leading/trailing whitespaces
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='sanjay',
+    author_email='sanjay007abd@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
